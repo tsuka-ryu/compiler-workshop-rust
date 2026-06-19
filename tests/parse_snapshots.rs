24dd729048daa62306b5b4522264a9a0f4dcce55
tsukaryu <drgon8otsuka@gmail.com>
2026-06-19 13:27:30 +0900

    add:  insta snapshot

--- tests/parse_snapshots.rs
+++ tests/parse_snapshots.rs
@@ -1,3 +1,4 @@
+use compiler_workshop::parse_resilient::parse_resilient;
 use compiler_workshop::parse_result::parse_result;
 use compiler_workshop::tokenize_span::tokenize_span;
 
@@ -39,4 +40,25 @@ fn t05_arrow_function() {
 fn t06_call_and_member() {
     let stmts = parse_result(tokenize_span("const y = f(a, b)[0];")).unwrap();
     insta::assert_debug_snapshot!(stmts);
+}
+
+#[test]
+fn t07_resilient_well_formed() {
+    // 正常入力: errors 空、panicked=false、parse_span と同じ木
+    let ret = parse_resilient(tokenize_span("const x = 1 + 2;"));
+    insta::assert_debug_snapshot!(ret);
+}
+
+#[test]
+fn t08_resilient_recoverable_missing_semicolon() {
+    // ; 欠落: 木は残り、errors に1件(recoverable tier)
+    let ret = parse_resilient(tokenize_span("const x = 5"));
+    insta::assert_debug_snapshot!(ret);
+}
+
+#[test]
+fn t09_resilient_fatal_discards_tree() {
+    // 式が壊れている: fatal。statements 空 + panicked=true + fatal 1件
+    let ret = parse_resilient(tokenize_span("const x = ;"));
+    insta::assert_debug_snapshot!(ret);
 }
